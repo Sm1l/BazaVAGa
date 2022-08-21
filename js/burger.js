ac83e99f16dd6fa8fa49f4941d82b3debd94c860
Sm1le <Onekonstantin@gmail.com>
2022-08-21 11:33:34 +0300

    закрытие бургер-меню при нажатии на ссылку

--- js/burger.js
+++ js/burger.js
@@ -1,12 +1,21 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".header__menu");
 const body = document.body;
+const links = document.querySelectorAll(".header__link");
 
-burger.addEventListener("click", burgerHandler);
+burger.addEventListener("click", burgerOnClick);
 
-function burgerHandler(evt) {
+function burgerOnClick(evt) {
   evt.preventDefault();
   burger.classList.toggle("active");
   menu.classList.toggle("active");
   body.classList.toggle("noscroll");
+}
+
+links.forEach((link) => link.addEventListener("click", closeOnClick));
+
+function closeOnClick() {
+  burger.classList.remove("active");
+  menu.classList.remove("active");
+  body.classList.remove("noscroll");
 }
